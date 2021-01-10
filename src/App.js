7a3199dc7bcbc5044eb74826d045bda0122711ed
Darek Kilian <emis@MacBook-Pro.local>
2021-01-10 19:39:15 +0100

    Changed image src

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const photos = [];
 
   for (let step = 1; step < 8; step++) {
-    photos.push(<img key={step} alt={step} src={`/assets/00${step}.jpg`} />);
+    photos.push(<img key={step} alt={step} src={`./assets/00${step}.jpg`} />);
   }
 
   return <Mansonry>{photos}</Mansonry>;
